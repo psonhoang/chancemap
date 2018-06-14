033a0bfba6d41c68cb020f459c5134819017e0be
Son Hoang Pham <pshoang102@gmail.com>
2018-06-14 11:35:48 +0700

    hoang.pham - thêm chức năng upload và display hình + upload resume_file

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -1,14 +1,54 @@
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
+const path = require('path');
 const bcrypt = require('bcryptjs');
 const passport = require('passport');
+const config = require('../config/database.js');
+// For file uploading
+const crypto = require('crypto');
+const multer = require('multer');
+const GridFsStorage = require('multer-gridfs-storage');
+const Grid = require('gridfs-stream');
 
 // Models
 const Account = require('../models/account');
 const User = require('../models/user');
 const Org = require('../models/org');
 
+// Database connection
+const connection = mongoose.connection;
+
+// Init gfs
+let gfs;
+connection.once('open', () => {
+	// Init stream
+	gfs = Grid(connection.db, mongoose.mongo);
+	gfs.collection('uploads');
+});
+
+// Create storage engine
+const storage = new GridFsStorage({
+  url: config.database,
+  file: (req, file) => {
+    return new Promise((resolve, reject) => {
+      crypto.randomBytes(16, (err, buf) => {
+        if (err) {
+          return reject(err);
+        }
+        const filename = buf.toString('hex') + path.extname(file.originalname);
+        const fileInfo = {
+          filename: filename,
+          bucketName: 'uploads'
+        };
+        resolve(fileInfo);
+      });
+    });
+  }
+});
+const upload = multer({ storage });
+
+// ***** ROUTES *****
 // Login
 router.get('/login', (req, res) => {
 	res.render('login', {title: "Code Dao | Login"});
@@ -33,6 +73,8 @@ router.get('/register', (req, res) => {
 	res.render('register', {title: "Code Dao | Sign up"});
 });
 
+// @route POST
+// @desc Determine which register view to render (regOrg || regUser)
 router.post('/register', (req, res) => {
 	let data = req.body;
 	if(!(data.org_acc || data.user_acc)) {
@@ -54,8 +96,12 @@ router.post('/register', (req, res) => {
 	}
 });
 
-router.post('/register/user', (req, res) => {
-	console.log(req.body);
+// @route POST
+// @desc Register new user account
+router.post('/register/user', upload.fields([{name: 'avatar', maxCount: 1}, {name: 'resume_file', maxCount: 1}]),
+(req, res) => {
+	// console.log(req.body);
+	// console.log(req.files);
 
 	let data = req.body;
 	let name = data.name;
@@ -65,11 +111,17 @@ router.post('/register/user', (req, res) => {
 	let interests = data.interests;
 	let skills = data.skills;
 	let resume_file = data.resume_file;
+	if(req.files['resume_file']) {
+		resume_file = 'files/' + req.files['resume_file'][0].filename;
+	}
 	let school = data.school;
 	let intro = data.intro;
 	let facebook = data.facebook;
 	let website = data.website;
-	let avatar = data.avatar;
+	let avatar = "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9b/Antu_im-invisible-user.svg/2000px-Antu_im-invisible-user.svg.png";
+	if(req.files['avatar']) {
+		avatar = 'files/' + req.files['avatar'][0].filename;
+	}
 
 	var newUser = new User({
 		_id: new mongoose.Types.ObjectId(),
@@ -123,8 +175,11 @@ router.post('/register/user', (req, res) => {
   });
 });
 
-router.post('/register/org', (req, res) => {
-	console.log(req.body);
+// @route POST
+// @desc Register new org account
+router.post('/register/org', upload.single('avatar'), (req, res) => {
+	// console.log(req.body);
+	// console.log(req.file);
 
 	let data = req.body;
 	let name = data.name;
@@ -135,7 +190,10 @@ router.post('/register/org', (req, res) => {
 	let desc = data.desc;
 	let facebook = data.facebook;
 	let website = data.website;
-	let avatar = data.avatar;
+	let avatar = "https://cdn0.iconfinder.com/data/icons/users-android-l-lollipop-icon-pack/24/group2-512.png";
+	if(req.file) {
+		avatar = 'files/' + req.file.filename;
+	}
 
 	var newOrg = new Org({
 		_id: new mongoose.Types.ObjectId(),

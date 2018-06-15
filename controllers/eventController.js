35f1da831cf12cb8492a7988d47a0a38cddadc33
Dang Nguyen <nmd.ptnk@gmail.com>
2018-06-15 15:02:11 +0700

    trying to fix error - dang

--- controllers/eventController.js
+++ controllers/eventController.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
+const Grid = require('gridfs-stream');
 
 const Event = require('../models/event');
 

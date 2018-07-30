1894763b048ce94b8eb128e52a629d33ac039dd0
SH Pham <pshoang102@gmail.com>
2018-07-31 06:48:12 +0700

    hoang.pham

--- public/app-assets/js/scripts/extensions/toastr.js
+++ public/app-assets/js/scripts/extensions/toastr.js
@@ -8,23 +8,23 @@
 	Author URL: http://www.themeforest.net/user/geekslabs
 ==========================================================================================*/
 $(document).ready(function(){
-
+	
 	// Success Type
 	$('#type-success').on('click',function(){
 		toastr.success('Have fun storming the castle!', 'Miracle Max Says');
 	});
 
-	// Success Type
+	// Info Type
 	$('#type-info').on('click',function(){
 		toastr.info('We do have the Kapua suite available.', 'Turtle Bay Resort');
 	});
 
-	// Success Type
+	// Warning Type
 	$('#type-warning').on('click',function(){
 		toastr.warning('My name is Inigo Montoya. You killed my father, prepare to die!');
 	});
 
-	// Success Type
+	// Error Type
 	$('#type-error').on('click',function(){
 		toastr.error('I do not think that word means what you think it means.', 'Inconceivable!');
 	});

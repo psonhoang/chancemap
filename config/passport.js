68e9b08b7f7c70a694e8f8ff29eaceeeea32326f
BaoVo <39vongocgiabao@gmail.com>
2018-09-05 12:05:48 +0700

    BaoVo

--- config/passport.js
+++ config/passport.js
@@ -4,29 +4,39 @@ const databaseConfig = require('./database');
 const bcrypt = require('bcryptjs');
 
 module.exports = function(passport) {
+  // check email
+  function validateEmail(username) {
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(username).toLowerCase());
+  }
   // Local Strategy
   passport.use(new LocalStrategy((username, password, done) => {
+    query = {username:username};
+    if (validateEmail(username)) {
+      var query = {email: username};
+    } else {
+      var query = {username: username};
+    }
     // Match username
-    let query = {username: username.trim()};
     Account.findOne(query, (err, account) => {
       if (err) throw err;
       if(!account) {
         return done(null, false, {message: 'No account found with username: '+ username});
       }
 
-      // Match password
+    // Match password
       bcrypt.compare(password, account.password, (err, isMatch) => {
         if(err) throw err;
-        if(isMatch) {
-          console.log("Matched pass!");
-          return done(null, account);
-        } else {
-          console.log("Wrong pass!");
-          return done(null, false, {message: 'Wrong password for ' + username});
-        }
+          if(isMatch) {
+            console.log("Matched pass!");
+            return done(null, account);
+          } else {
+            console.log("Wrong pass!");
+            return done(null, false, {message: 'Wrong password for ' + username});
+          }
+        });
       });
-    });
-  }));
+    }));
 
   passport.serializeUser(function(account, done) {
     done(null, account.id);
